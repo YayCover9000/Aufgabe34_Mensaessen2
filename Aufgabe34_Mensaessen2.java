76425e71ecf630672165e5b42c45a5cb5ecd9dd5
YayCover9000 <yassuanf@googlemail.com>
2023-11-27 14:18:50 +0100

    if nicht richtige eingabe dann nur ein output

--- Aufgabe34_Mensaessen2.java
+++ Aufgabe34_Mensaessen2.java
@@ -3,13 +3,15 @@ public class Aufgabe34_Mensaessen2 {
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
 		int eingabe = sc.nextInt();
-		String ausgabe = "Tja hier gibts nicht zu essen";
+		String ausgabe = null;
 		if(eingabe > 4 || eingabe < 1) {
-			System.out.println("auswahl nur zwichen 1 und 4 erlaubt");
+			System.out.println("Essensangebot-Nr. "+eingabe+" gibt es leider in der Mensa nicht.");
 		} else {
 			ausgabe = Mensaessen_berechnen(eingabe);
 		}
-		System.out.println("Auswahl: " + ausgabe);
+		if(ausgabe != null ) {
+			System.out.println("Auswahl: " + ausgabe);
+		}
 	}
 	private static String Mensaessen_berechnen(int eingabe) {
 		String[] array = {"Tomatensuppe","Apfelmusslosbrudi","Marzipantorte","Karottenpudding"};
